0b0af1483b4d4c2d5147cb2e9c4634561368dc53
Graham Daws <graham.daws@gmail.com>
2012-07-09 23:33:12 +0100

    pre-defined symbols defined by 1.7 spec

--- asm/symbol_table.cpp
+++ asm/symbol_table.cpp
@@ -4,19 +4,48 @@
 void load_predefined_symbols(symbol_table & st){
     
     static const char * basic_instructions[] = {
-        "SET", "ADD", "SUB", "MUL", "DIV", "MOD", "SHL", "SHR", "AND", "BOR", "XOR", "IFE", "IFN", "IFG", "IFB"
+        "SET", "ADD", "SUB", "MUL", "DIV", "DVI",
+        "MOD", "MDI", "AND", "BOR", "XOR", "SHR", 
+        "ASR", "SHL", "IFB", "IFC", "IFE", "IFN", 
+        "IFG", "IFA", "IFL", "IFU", "ADX", "SBX", 
+        "STI", "STD"
     };
     
     for(unsigned int i = 0; i < sizeof(basic_instructions)/sizeof(char *); i++){
+    
         st[lexer_string(basic_instructions[i])] = std::make_tuple(SYMBOL_TYPE_BASIC_INSTRUCTION, i + 1);
+        
+        // jump the reserved value gaps
+        switch(i){
+            case 0x17:
+                i = 0x19;
+                break;
+            case 0x1b:
+                i = 0x1d;
+                break;
+            default:
+                break;
+        }
     }
     
     static const char * nonbasic_instructions[] = {
-        "JSR"  
+        "JSR", "INT", "IAG", "IAS", "RFI", "IAQ", "HWN", "HWQ", "HWI"
     };
     
     for(unsigned int i = 0; i < sizeof(nonbasic_instructions)/sizeof(char *); i++){
+        
         st[lexer_string(nonbasic_instructions[i])] = std::make_tuple(SYMBOL_TYPE_NONBASIC_INSTRUCTION, i + 1);
+        
+        // jump the reserved value gaps
+        switch(i){
+            case 0x01:
+                i = 0x07;
+                break;
+            case 0x0c:
+                i = 0x0f;
+            default:
+                break;
+        }
     }
     
     static const char * general_registers[] = {
@@ -29,21 +58,17 @@ void load_predefined_symbols(symbol_table & st){
         st[lexer_string(general_registers[i])] = std::make_tuple(SYMBOL_TYPE_GENERAL_REGISTER, i);
     }
     
+    st[lexer_string("PUSH")] = std::make_tuple(SYMBOL_TYPE_STACK_OPERATION, 0x18);
+    st[lexer_string("POP")] = std::make_tuple(SYMBOL_TYPE_STACK_OPERATION, 0x18);
+    st[lexer_string("PEEK")] = std::make_tuple(SYMBOL_TYPE_STACK_OPERATION, 0x19);
+    st[lexer_string("PICK")] = std::make_tuple(SYMBOL_TYPE_STACK_OPERATION, 0x1a);
+    
     static const char * special_registers[] = {
-        "SP", "PC", "O"  
+        "SP", "PC", "EX"  
     };
     
     for(unsigned int i = 0; i < sizeof(special_registers)/sizeof(char *); i++){
         st[lexer_string(special_registers[i])] = std::make_tuple(SYMBOL_TYPE_SPECIAL_REGISTER, i + 0x1b);
     }
-    
-    static const char * stack_operations[] = {
-        "POP", "PEEK", "PUSH"  
-    };
-    
-    for(unsigned int i = 0; i < sizeof(stack_operations)/sizeof(char *); i++){
-        st[lexer_string(stack_operations[i])] = std::make_tuple(SYMBOL_TYPE_STACK_OPERATION, i + 0x18);
-    }
-    
 }
 
